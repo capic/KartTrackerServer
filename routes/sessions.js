580e38a10181867d0242a50ae7c718974140f4a1
Vincent <vcapic@gmail.com>
2016-04-06 21:48:47 +0200

    correction du chemin de tracksajout des sessions

--- routes/sessions.js
+++ routes/sessions.js
@@ -3,6 +3,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+router.get('/', function(req, res, next) {
+  var callback = function (action) {
+    res.json(action);
+  };
+
+  var relationsList = [
+    {model: models.GpsData, as: 'gps_data'},
+    {model: models.Track, as: 'track'}
+  ];
+
+  models.Action.findAll({
+    include: [
+      {model: models.GpsData, as: 'gps_data'},
+      {model: models.Track, as: 'track'}
+    ]}).then(callback)
+      .catch(
+          function (errors) {
+            console.log(errors);
+          }
+      );
+});
+
 router.post('/', function(req, res, next) {
   var sessionsList = JSON.parse(JSON.stringify(req.body));
 
