acebf2b9c5af89b5a3bb77ab011957f0922a00b2
Vincent <vcapic@gmail.com>
2016-04-10 23:22:18 +0200

    test

--- common/utils.js
+++ common/utils.js
@@ -9,7 +9,7 @@ utils.executeAction = function (action, parametersList) {
     var options = {
         mode: 'text',
         scriptPath: "/home/pi/KartTracker/",
-        args: parametersList
+        args: 1
     };
 
     PythonShell.run(action, options, function (err, results) {
