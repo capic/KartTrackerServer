9772dadd24092a23fe304a625784550ca88312eb
Vincent <vcapic@gmail.com>
2016-04-13 07:46:13 +0200

    on recupere la liste des ids inseres pour voir si tout s'est bien passe

--- routes/sessions.js
+++ routes/sessions.js
@@ -3,42 +3,53 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  var callback = function (action) {
-    res.json(action);
-  };
+router.get('/', function (req, res, next) {
+    var callback = function (action) {
+        res.json(action);
+    };
 
-  var relationsList = [
-    {model: models.GpsData, as: 'gps_datas'},
-    {model: models.Track, as: 'track'}
-  ];
+    var relationsList = [
+        {model: models.GpsData, as: 'gps_datas'},
+        {model: models.Track, as: 'track'}
+    ];
 
-  models.Session.findAll({
-    include: [
-      {model: models.GpsData, as: 'gps_datas'},
-      {model: models.Track, as: 'track'}
-    ]}).then(callback)
-      .catch(
-          function (errors) {
-            console.log(errors);
-          }
-      );
+    models.Session.findAll({
+        include: [
+            {model: models.GpsData, as: 'gps_datas'},
+            {model: models.Track, as: 'track'}
+        ]
+    }).then(callback)
+        .catch(
+            function (errors) {
+                console.log(errors);
+            }
+        );
 });
 
-router.post('/', function(req, res, next) {
-  if (req.body.hasOwnProperty('datas')) {
-    var sessionsList = JSON.parse(req.body.datas);
+router.post('/', function (req, res, next) {
+    if (req.body.hasOwnProperty('datas')) {
+        var sessionsList = JSON.parse(req.body.datas);
+        var sessionIdsList = [];
 
-    sessionsList.forEach(function(session) {
-      console.log(session);
-      models.Session.create(session, {
-        include: [
-          {model: models.GpsData, as: 'gps_datas'}
-        ]
-      });
-    });
-    res.end();
-  }
+        sessionsList.forEach(function (session) {
+            // on sauvegarde l'id de la session pour savoir quelles sessions viennent d'être insérer pour vérifier que tout
+            // à bien été correctement traité en base
+            sessionIdsList.push(session.id);
+
+            models.Session.create(session, {
+                include: [
+                    {model: models.GpsData, as: 'gps_datas'}
+                ]
+            })
+        });
+
+        models.sequelize.query("SELECT id FROM session WHERE id in :idsList", {
+            replacements: {idsList: sessionIdsList},
+            type: models.sequelize.QueryTypes.SELECT
+        }).then(function(list) {
+            res.json(list);
+        })
+    }
 });
 
 module.exports = router;
