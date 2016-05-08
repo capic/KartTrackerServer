9f14f19f16cb1d0e97463f269af1e9561d58d007
Vincent <vcapic@gmail.com>
2016-05-08 12:10:29 +0200

    ajout d'une méthode pour récupérer des informations complémentaires au circuit

--- routes/tracks.js
+++ routes/tracks.js
@@ -20,7 +20,7 @@ router.get('/withInfos', function(res, res, next) {
     var today = new Date();
 
     trackModelList.forEach(function(track) {
-      models.Session.count({where: {track_id: track.id, date_session: models.sequelize.fn('date', "now") }}).then(function(result) {
+      var promise = models.Session.count({where: {track_id: track.id, date_session: models.sequelize.fn('date', "now") }}).then(function(result) {
         track.dataValues.sessions_count_today = result;
 
         var promise = models.Session.count({where: {track_id: track.id}}).then(function(result) {
@@ -30,6 +30,8 @@ router.get('/withInfos', function(res, res, next) {
 
         promises.push(promise);
       });
+
+      promises.push(promise);
     });
 
     Promise.all(promises).then(function() {
